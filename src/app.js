0fb7537c999384cba5239b2742e4d35192a94c73
innyso <mini.inny@gmail.com>
2017-08-07 00:32:55 +1000

    WIP: Allow user to update the hashtag
    First version working but need to tidy up the code.

--- src/app.js
+++ src/app.js
@@ -3,10 +3,13 @@ var env = require('dotenv').config().parsed || process.env;
 var streamError = require('./streams/error');
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var pug = require('pug');
 var currentHashtag = env.CURRENT_HASHTAG;
 
 app.set('port', (env.PORT || 5000));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 var client = new Twitter({
   consumer_key: env.CONSUMER_KEY,
@@ -15,16 +18,47 @@ var client = new Twitter({
   access_token_secret: env.ACCESS_TOKEN_SECRET
 });
 
-var streamParameters = {
-  track: currentHashtag
-};
-
+var stream = null;
 var tweetCounter = 0;
+var TwitWords = [];
+var initialized = false;
 
-client.stream('statuses/filter', streamParameters, function (stream) {
-  stream.on('data', () => {tweetCounter++})
-  stream.on('error', streamError);
+// Tracker function
+function TrackWords(array){
+  if (initialized) {
+    console.log("=====================");
+    console.log("destroyyyyyyy");
+    stream.destroy();
+  }
+
+  console.log("==============");
+  console.log(array.toString());
+  stream = client.stream('statuses/filter',{track:array.toString()});
+  stream.on('data', function(event) {
+  console.log(event && event.text);
 });
+
+  stream.on('error', function(error) {
+    throw error;
+  });
+  initialized = true;
+}
+
+// Add word
+function AddTwitWord(word){
+  if(TwitWords.indexOf(word)==-1){
+    TwitWords.push(word);
+    TrackWords(TwitWords);
+  }
+}
+
+// Remove word
+function RemoveTwitWord(word){
+  if(TwitWords.indexOf(word)!=-1){
+    TwitWords.splice(TwitWords.indexOf(word),1);
+    TrackWords(TwitWords);
+  }
+}
 
 app.get('/count', function (req, res) {
   res.send(tweetCounter.toString());
@@ -35,7 +69,16 @@ app.get('/reset', function(req, res) {
   res.sendStatus(200);
 });
 
+app.post('/updateHashTag',function(req, res) {
+  AddTwitWord(req.body.hashTag);
+  RemoveTwitWord(currentHashtag);
+  currentHashtag = req.body.hashTag;
+  tweetCounter = 0;
+  res.sendStatus(200);
+});
+
 app.get('/', function (req, res) {
+  AddTwitWord(currentHashtag);
   welcomeHTML = pug.renderFile(__dirname + '/html/welcome.pug',{currentHashtag})
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write(welcomeHTML);
